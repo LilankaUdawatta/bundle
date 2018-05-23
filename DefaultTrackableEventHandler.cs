c23e087adeadccfdad4635543c9d21883b03692c
LilankaUdawatta <39363703+LilankaUdawatta@users.noreply.github.com>
2018-05-23 16:08:51 +0530

    Add files via upload
    loads cloud data related to meta vuforia, but enabling and disabling instanciated objects is still a problem

--- DefaultTrackableEventHandler.cs
+++ DefaultTrackableEventHandler.cs
@@ -12,6 +12,7 @@ using Vuforia;
 using System.Collections;
 using System.IO;
 using System;
+using System.Collections.Generic;
 using UnityEngine.Networking;
 //added
 
@@ -45,11 +46,26 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
 	//Variable "loadedAsset" to load prefab as gameobject
 	private GameObject loadedAsset;	
     // AssetBundle Url
-    public string url = "https://firebasestorage.googleapis.com/v0/b/arapp-ba619.appspot.com/o/Editor%2F2_Game%2F2?alt=media&token=38c7179e-6bff-4dc2-aded-89001c842dbd";
+    public string url = "Load";
+    private string url_loaded = "Unload";
+    private bool loded;
+    private WWW www;
+    //private List<GameObject> _instance;
+    private GameObject[] _instance;
+    private GameObject clone;
+    
+    //load CloudRecoBehaviour <type> 
+    //must include using vuforia
+    public CloudRecoBehaviour CloudRecognitionGameObject;
+    // load SimpleCloudHandler1 script from CloudRecoBehaviour attached
+    private SimpleCloudHandler1 CloudHandlerScript;
+
     //Added
 
     protected virtual void Start()
     {
+        // (re-)initialize as empty list
+        //WWW www = new WWW();
         mTrackableBehaviour = GetComponent<TrackableBehaviour>();
         if (mTrackableBehaviour)
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
@@ -117,43 +133,115 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         foreach (var component in canvasComponents)
             component.enabled = true;
 
-        StartCoroutine (DownloadAssetBundles());    
+
+        //download assetbundles
+        if (String.Equals(url_loaded,url))
+        {
+            //StartCoroutine (LoadAsse());
+            LoadAsse(); 
+        }
+
+        else
+        {
+            StartCoroutine (DownloadAsse());   
+        }   
     }
 
 //Added
-    IEnumerator DownloadAssetBundles()
-    {
-		//download AssetBundle
-        using (WWW www = new WWW(url))
+    
+    IEnumerator DownloadAsse()
+    {   
+        i = 0;
+        // Frees the memory from the web stream
+       // www.Dispose();
+        // Store Previous loaded url
+        //url_loaded = url;
+        _instance = new GameObject[20];
+
+        //Load "SimpleCloudHandler1" from "CloudRecoBehaviour"
+        CloudHandlerScript = CloudRecognitionGameObject.GetComponent<SimpleCloudHandler1>();
+        // access "ReadMeta()" function from "SimpleCloudHandler1" script
+        url = CloudHandlerScript.ReadMeta();
+        Debug.Log("Downloading from yes yes worked:"+ url);
+        // Update url_loaded to prevent downloading assets again for the reco
+        url_loaded = url;
+        //download AssetBundle
+        www = new WWW(url);
+        // using (www = new WWW(url)) -- Or use this
+        using (www)
         {
-			//wait for download
+            //wait for download
             yield return www;
 
-			Debug.Log ("Loaded ");
-        	if (www.error != null)
+            Debug.Log ("Loaded ");
+            if (www.error != null)
             throw new Exception ("WWW download had an error: " + www.error);
 
-			asseBundle = www.assetBundle;
+            asseBundle = www.assetBundle;
 
-			//load all assets
-			_assets = www.assetBundle.LoadAllAssets();
-			//load all asset names
-			assetNames = www.assetBundle.GetAllAssetNames();
-			
-			foreach (UnityEngine.Object eachAsset in _assets)
-			{
-				// request prefab to load
-				asset = asseBundle.LoadAssetAsync<GameObject>(assetNames[i]);
-				yield return asset;
-				//load prefab as gameobject
-				loadedAsset = asset.asset as GameObject;
-				//Instanciate Prefab
-				Instantiate(asseBundle.LoadAsset(assetNames[i]));
-				i++;
-			}
-        }
+             //load all assets
+            _assets = www.assetBundle.LoadAllAssets();
+            //load all asset names
+            assetNames = www.assetBundle.GetAllAssetNames();
+                
+            foreach (UnityEngine.Object eachAsset in _assets)
+            {
+                // request prefab to load
+                asset = asseBundle.LoadAssetAsync<GameObject>(assetNames[i]);
+                yield return asset;
+                //load prefab as gameobject
+                 loadedAsset = asset.asset as GameObject;
+                 //Instanciate Prefab
+                //Instantiate(asseBundle.LoadAsset(assetNames[i]));
+                    //GameObject clone = (GameObject)Instantiate(asseBundle.LoadAsset(assetNames[i]));
+                //_instance[i] = clone;
+                //_instance.Add(clone);
+                GameObject clone = new GameObject();
+                clone = Instantiate(asseBundle.LoadAsset(assetNames[i])) as GameObject;
+                _instance[i] = clone;
+                //clone.transform.parent = mTrackableBehaviour.transform;
+
+                //Destory(clone);
+                //_instance.Add((GameObject)Instantiate(asseBundle.LoadAsset(assetNames[i])));    
+                i++;
+            }   
+        } 
     }
-    //Added
+
+   /* IEnumerator LoadAsse()
+    {
+        //load all assets
+            i = 0;
+            //_instance = new List<GameObject>();
+            _instance = new GameObject[20];
+                _assets = www.assetBundle.LoadAllAssets();
+                //load all asset names
+                assetNames = www.assetBundle.GetAllAssetNames();
+                
+                foreach (UnityEngine.Object eachAsset in _assets)
+                {
+                    // request prefab to load
+                    asset = asseBundle.LoadAssetAsync<GameObject>(assetNames[i]);
+                    yield return asset;
+                    //load prefab as gameobject
+                    loadedAsset = asset.asset as GameObject;
+                    //Instanciate Prefab
+                    //Instantiate(asseBundle.LoadAsset(assetNames[i]));
+                    GameObject clone = new GameObject();
+                    clone = (GameObject)Instantiate(asseBundle.LoadAsset(assetNames[i]));
+                    _instance[i] = clone;
+                    //_instance.Add(clone);
+                    //_instance.Add((GameObject)Instantiate(asseBundle.LoadAsset(assetNames[i])));    
+                    i++;
+                }
+                
+    }*/
+
+    private void LoadAsse()
+    {
+        EnableObjects();
+    }
+
 
     protected virtual void OnTrackingLost()
     {
@@ -172,6 +260,39 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         // Disable canvas':
         foreach (var component in canvasComponents)
             component.enabled = false;
+
+         //asseBundle.Unload(false);
+
+       /* i=0;
+        foreach (UnityEngine.Object eachAsset in _instance)
+        {
+            Destroy(_instance[i]);
+            i++;
+        }*/
+        //disable from play.. on refound, enable 
+        // on new, destory and proceed
+        
+       //DisableObjects();
+    }
+
+    private void EnableObjects()
+    {
+        i = 0;
+        foreach (UnityEngine.Object eachAsset in _assets)
+        {
+           _instance[i].SetActive(true);
+            i++;
+        }
+    }
+
+    private void DisableObjects()
+    {
+        i = 0;
+        foreach (UnityEngine.Object eachAsset in _assets)
+        {
+            _instance[i].SetActive(false);
+            i++;
+        }
     }
 
     #endregion // PROTECTED_METHODS
